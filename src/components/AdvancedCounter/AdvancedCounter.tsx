2198392e82f17e8fe7bf9fb706011be67dc904ed
Robb-Designs <rtlee@aggies.ncat.edu>
2026-04-30 11:35:38 -0400

    import useState: Set up count state and 2 handler functions

--- src/components/AdvancedCounter/AdvancedCounter.tsx
+++ src/components/AdvancedCounter/AdvancedCounter.tsx
@@ -2,8 +2,22 @@ import {useState} from 'react';
 
 
 export default function AdvancedCounter() {
+    const [count, setCount] = useState(0); //Set initial default state to 0
+    
+    // Adding 
+    function handleAdd(){
+        setCount(count + 1);
+        console.log(count)
+    }
+
+    // Subtracting
+    function handleSub(){
+        setCount(count -1);
+        console.log(count)
+    }
 
     return(
+    
 
     );
 }
