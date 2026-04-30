d1f85adaf471904ca7b8ec0a197242b806b38515
Robb-Designs <rtlee@aggies.ncat.edu>
2026-04-30 14:56:56 -0400

    Add step useState and replaced inside handler functions

--- src/components/AdvancedCounter/AdvancedCounter.tsx
+++ src/components/AdvancedCounter/AdvancedCounter.tsx
@@ -1,33 +1,30 @@
-import {useState, useEffect} from 'react';
-
+import { useState, useEffect } from "react";
 
 export default function AdvancedCounter() {
-    const [count, setCount] = useState(0); //Set initial default state to 0
-    
-    // Adding 
-    function handleAdd(){
-        setCount(prev => prev + 1); //use what React knows the value is, better than setCount(count + 1)
-      
-    }
+  const [count, setCount] = useState(0); //Set initial default state to 0
+  const [step, setStep] = useState(1);
 
-    // Subtracting
-    function handleSub(){
-        setCount(prev => prev - 1);
-       
-    }
+  // Adding
+  function handleAdd() {
+    setCount((prev) => prev + step); //use what React knows the value is, better than setCount(count + 1)
+  }
 
+  // Subtracting
+  function handleSub() {
+    setCount((prev) => prev - step);
+  }
 
-    useEffect(() => {}, [count])
+  useEffect(() => {
+    console.log('Rendered Count from useEffect:', count);
+  }, [count]);
 
-    return(
-    
-        <div>
-            <h1>{count}</h1>
-            <div>
-                <button onClick={handleAdd}>+ Add</button>
-                <button onClick={handleSub}>- Sub</button>
-            </div>
-        </div>
-
-    );
+  return (
+    <div>
+      <h1>{count}</h1>
+      <div>
+        <button onClick={handleAdd}>+ Add</button>
+        <button onClick={handleSub}>- Sub</button>
+      </div>
+    </div>
+  );
 }
